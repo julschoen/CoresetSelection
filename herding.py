6984471c370b33c09467716bb9ab55a2a2273294
julschoen <julianschoen@gmx.de>
2023-02-24 14:47:44 +0100

    Update herding.py

--- herding.py
+++ herding.py
@@ -78,35 +78,36 @@ def herding_resnet():
     resnet = torch.nn.Sequential(*(list(resnet.children())[:-1])).eval()
 
     with torch.no_grad():
-        S = torch.zeros((args.num_classes*args.num_ims, 3, 32, 32), dtype=float, device=args.device)
-        for c in range(args.num_classes):
-            X = torch.load(os.path.join('../data/', f'data_class_{c}.pt')).to(args.device)
-            # Extract features from the dataset using the ResNet model
-            X_features = resnet(X)
+        with torch.autocast(device_type=self.p.device, dtype=torch.float16):
+            S = torch.zeros((args.num_classes*args.num_ims, 3, 32, 32), dtype=float, device=args.device)
+            for c in range(args.num_classes):
+                X = torch.load(os.path.join('../data/', f'data_class_{c}.pt')).to(args.device)
+                # Extract features from the dataset using the ResNet model
+                X_features = resnet(X)
 
-            # Compute the empirical mean of the dataset
-            mu = X_features.mean(dim=0)
+                # Compute the empirical mean of the dataset
+                mu = X_features.mean(dim=0)
 
-            # Initialize the set of selected points and the set of unselected points
-            U = X.clone()
+                # Initialize the set of selected points and the set of unselected points
+                U = X.clone()
 
-            # Iteratively select the coreset points
-            for i in range(args.num_ims):
-                # Extract features from the unselected points using the ResNet model
-                U_features = resnet(U)
+                # Iteratively select the coreset points
+                for i in range(args.num_ims):
+                    # Extract features from the unselected points using the ResNet model
+                    U_features = resnet(U)
 
-                # Compute the similarity between the unselected points and the empirical mean based on the features
-                sim = F.cosine_similarity(U_features, mu.view(1, -1), dim=1)
+                    # Compute the similarity between the unselected points and the empirical mean based on the features
+                    sim = F.cosine_similarity(U_features, mu.view(1, -1), dim=1)
 
-                # Find the index of the unselected point with the highest similarity
-                j = torch.argmax(sim)
+                    # Find the index of the unselected point with the highest similarity
+                    j = torch.argmax(sim)
 
-                # Add the selected point to the coreset and remove it from the set of unselected points
-                S[(args.num_ims*c)+i] = U[j]
-                U = torch.cat((U[:j], U[j+1:]))
+                    # Add the selected point to the coreset and remove it from the set of unselected points
+                    S[(args.num_ims*c)+i] = U[j]
+                    U = torch.cat((U[:j], U[j+1:]))
 
-                # Update the empirical mean based on the selected points
-                mu = resnet(S[:(args.num_ims*c)+i+1].mean(dim=0).unsqueeze(0)).squeeze(0)
+                    # Update the empirical mean based on the selected points
+                    mu = resnet(S[:(args.num_ims*c)+i+1].mean(dim=0).unsqueeze(0)).squeeze(0)
     log_interpolation(S, args)
     
 
