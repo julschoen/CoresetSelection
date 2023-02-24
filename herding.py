d80306dc13f6466ea35b677798229cf36b37062c
julschoen <julianschoen@gmx.de>
2023-02-24 14:59:25 +0100

    Update herding.py

--- herding.py
+++ herding.py
@@ -107,7 +107,7 @@ def herding_resnet():
                 U = torch.cat((U[:j], U[j+1:]))
 
                 # Update the empirical mean based on the selected points
-                mu = resnet(S[:(args.num_ims*c)+i+1].mean(dim=0).unsqueeze(0)).squeeze(0)
+                mu = resnet().mean(dim=0)
     log_interpolation(S, args)
     
 
