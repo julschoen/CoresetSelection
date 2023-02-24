fb43f35d85415aa4a00fd6320cb3f6f9646fd25c
julschoen <julianschoen@gmx.de>
2023-02-24 14:43:34 +0100

    Update herding.py

--- herding.py
+++ herding.py
@@ -78,7 +78,7 @@ def herding_resnet():
     resnet = torch.nn.Sequential(*(list(resnet.children())[:-1])).eval()
 
     with torch.no_grad():
-        S = torch.zeros((args.num_classes*args.num_ims, 3, 32, 32), dtype=X.dtype, device=args.device)
+        S = torch.zeros((args.num_classes*args.num_ims, 3, 32, 32), dtype=float, device=args.device)
         for c in range(args.num_classes):
             X = torch.load(os.path.join('../data/', f'data_class_{c}.pt'))
             # Extract features from the dataset using the ResNet model
