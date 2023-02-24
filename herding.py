7f9437c1e7918d2df6657224e2441b2e8fd0c5d5
julschoen <julianschoen@gmx.de>
2023-02-24 14:50:35 +0100

    Update herding.py

--- herding.py
+++ herding.py
@@ -93,8 +93,7 @@ def herding_resnet():
             # Iteratively select the coreset points
             for i in range(args.num_ims):
                 # Extract features from the unselected points using the ResNet model
-                U_features = resnet(U)
-                print(U_features.shape)
+                U_features = resnet(U).squeeze()
                 # Compute the similarity between the unselected points and the empirical mean based on the features
                 sim = F.cosine_similarity(U_features, mu.view(1, -1), dim=1)
 
